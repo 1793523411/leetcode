770415c46120cf7a5b454eb10582289b96b3f24f
ygjzs <1793523411@qq.com>
2020-11-16 20:18:18 +0800

    three

---
+++ three/lc26/Solution.java
@@ -0,0 +1,19 @@
+package lc26;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 1;
+        while (q < nums.length) {
+            if (nums[p] != nums[q]) {
+                if (q - p > 1) {
+                    nums[p + 1] = nums[q];
+                }
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+    }
+}
---
+++ three/lc328/Solution.java
@@ -0,0 +1,46 @@
+package lc328;
+
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        // 分别定义奇偶链表的 虚拟头结点 和 尾结点
+        ListNode oddHead = new ListNode();
+        ListNode oddTail = oddHead;
+        ListNode evenHead = new ListNode();
+        ListNode evenTail = evenHead;
+        // 遍历原链表，根据 isOdd 标识位决定将当前结点插入到奇链表还是偶链表（尾插法）
+        boolean isOdd = true;
+        while (head != null) {
+            if (isOdd) {
+                oddTail.next = head;
+                oddTail = oddTail.next;
+            } else {
+                evenTail.next = head;
+                evenTail = evenTail.next;
+            }
+            head = head.next;
+            isOdd = !isOdd;
+        }
+        // 将奇链表后面拼接上偶链表，并将偶链表的next设置为null
+        oddTail.next = evenHead.next;
+        evenTail.next = null;
+        return oddHead.next;
+    }
+}
